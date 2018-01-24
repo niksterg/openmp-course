da612272c1defb4344c873c56a7b9e236f05fa9e
Nikolaos Stergioulas <niksterg1@gmail.com>
2018-01-24 23:21:42 +0200

    Create table-sum.c

---
+++ table-sum.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <omp.h>
+
+#define N 10
+
+int main(void) {
+
+int i,
+    sum,
+    a[N];
+
+ for(i=0;i<N;i++) a[i] = i;
+
+ sum = 0;
+
+#pragma omp parallel for shared(a) private(i) \
+                     reduction(+:sum) \
+                     default(none)
+ for(i=0;i<N;i++) sum += a[i];
+
+ printf("sum = %d\n",sum);
+
+return 0;
+
+}
