da6ae5e0da71c8aea5d0112abf6f965141023bd3
Nikolaos Stergioulas <niksterg1@gmail.com>
2019-01-25 16:08:22 +0200

    Now only master thread reads the number of threads.

--- hello-world-parallel-id-scope.c
+++ hello-world-parallel-id-scope.c
@@ -9,9 +9,10 @@ int myid,
 #pragma omp parallel shared(nThreads) private(myid) default(none)
 {
  myid = omp_get_thread_num();
- nThreads = omp_get_num_threads();
 
  printf("Thread %d says: Hello World!\n",myid);
+
+ if(myid==0) nThreads = omp_get_num_threads();
 }
 
  printf("The total number of threads used was %d.\n",nThreads);
